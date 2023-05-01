695f1586f7e940ec7e3f2effa01f3f9a74f9187e
Norberto Rodriguez Vaquero <norberto.rodriguezvaquero@gmail.com>
2023-05-01 18:24:32 -0300

    mass

--- TP 2/PARTE 4/2.4.20.c
+++ TP 2/PARTE 4/2.4.20.c
@@ -23,9 +23,8 @@ la siguiente declaración: void porcentajes(float total, float individual, float
 *porcentaje);
 */
 
-// INCOMPLETO
-
 #include <stdio.h>
+#include <stdlib.h>
 
 typedef struct {
     int dni;
@@ -33,47 +32,85 @@ typedef struct {
     char medio_pago;
 } venta;
 
-int ingresarVenta(venta *);
+void ingresarVenta(venta *, int *, int *, int *);
 float totalVenta(venta *una_venta, float totalT, float *totalE, float *totalC);
 void ventaMayor(venta ventaIngresada, venta *ventaMayor);
 void porcentajes(float total, float individual, float *porcentaje);
 
 int main(int argc, char *argv[]) {
-    venta ventaIngresada;
+    venta ventaIngresada, ventasMayor = {0, 99999999, 'x'};
+    char inicio;
+    float totalT = 0, totalE = 0, totalC = 0, TOTAL;
+    float porcentajeT, porcentajeE, porcentajeC;
+    int contT = 0, contE = 0, contC = 0;
+    int finalizar = 0;
 
     do {
-        if (ingresarVenta(&ventaIngresada))
+        do {
+            // Inicio y finalización del programa.
+            printf("\nOprima > Enter < para ingresar venta. > F < para finalizar el programa...\n\n");
+            inicio = getchar();
+            if (inicio == 'F' || inicio == 'f')
+                finalizar = 1;
+            else if (inicio != '\n')
+                getchar();
+        } while (inicio != '\n');
+
+        if (finalizar)
             break;
 
+        ingresarVenta(&ventaIngresada, &contT, &contE, &contE);
+        totalT = totalVenta(&ventaIngresada, totalT, &totalE, &totalC);
+        ventaMayor(ventaIngresada, &ventasMayor);
     } while (1);
+
+    // Hago el cálculo de los porcentajes de cada medio de pago.
+    TOTAL = totalT + totalE + totalC;
+    porcentajes(TOTAL, totalT, &porcentajeT);
+    porcentajes(TOTAL, totalE, &porcentajeE);
+    porcentajes(TOTAL, totalC, &porcentajeC);
+
+    // Muestro resultados por pantalla.
+    // Total por cada medio de pago...
+    printf("\n> El monto TOTAL del día es de %.2f.", TOTAL);
+    printf("\n\n> El monto pagado por tarjeta es de %.2f.\n> El monto pagado en efectivo es de %.2f.\n> El monto pagado por cuenta corriente es de %.2f.\n\n", totalT, totalE, totalC);
+
+    // La mayor venta ingresada...
+    printf(
+        "> La mayor venta ingresada fue de $%.2f con la opción de pago %c, por el cliente de dni %i", ventasMayor.monto, ventasMayor.medio_pago, ventasMayor.dni);
+
+    // Porcentaje de ingresos por cada medio de pago...
+    printf("\n\n> El porcentaje de ventas por cada medio de pago fue de...\n> Tarjetas: %.2f%%\n> Efectivo: %.2f%%\n> Cuenta corriente: %.2f%%", porcentajeT, porcentajeE, porcentajeC);
+
+    printf("\n\nGracias por usar este programa...\n\n");
+    return 0;
 }
 
-int ingresarVenta(venta *ventaIngreso) {
+// función para ingresar venta con parámetro por referencia y devuelve un booleano dependiendo de si se ingresa 'F' o no.
+void ingresarVenta(venta *ventaIngreso, int *contT, int *contE, int *contC) {
     do {
         printf("Ingresar DNI: ");
         scanf("%i", &ventaIngreso->dni);
-
-        if (ventaIngreso->dni == 'F')
-            return 1;
     } while (ventaIngreso->dni < 1 || ventaIngreso->dni > 99999999);
 
     do {
         printf("Ingresar monto: ");
         scanf("%f", &ventaIngreso->monto);
-
-        if (ventaIngreso->monto == 'F')
-            return 1;
     } while (ventaIngreso->monto < 1);
 
     do {
-        printf("Ingresar medio de pago (e para efectivo, c para cuenta corriente y d para tarjeta de crédito o débito): ");
+        getchar();
+        printf("Ingresar medio de pago ('t' para crédito o débito, 'e' para efectivo y 'c' para cuenta corriente): ");
         ventaIngreso->medio_pago = getchar();
-
-        if (ventaIngreso->medio_pago == 'F')
-            return 1;
+        getchar();
     } while (ventaIngreso->medio_pago != 't' && ventaIngreso->medio_pago != 'c' && ventaIngreso->medio_pago != 'e');
 
-    return 0;
+    if (ventaIngreso->medio_pago == 't')
+        *contT++;
+    else if (ventaIngreso->medio_pago == 'e')
+        *contE++;
+    else
+        *contC++;
 }
 
 float totalVenta(venta *una_venta, float totalT, float *totalE, float *totalC) {
@@ -83,11 +120,16 @@ float totalVenta(venta *una_venta, float totalT, float *totalE, float *totalC) {
         *totalE += una_venta->monto;
     else
         *totalC += una_venta->monto;
+
+    return totalT;
 }
 
 void ventaMayor(venta ventaIngresada, venta *ventaMayor) {
-    if (ventaIngresada.monto > ventaMayor->monto)
+    if (ventaIngresada.monto > ventaMayor->monto) {
         ventaMayor->monto = ventaIngresada.monto;
+        ventaMayor->dni = ventaIngresada.dni;
+        ventaMayor->medio_pago = ventaIngresada.medio_pago;
+    }
 }
 
 void porcentajes(float total, float individual, float *porcentaje) {
--- TP 2/PARTE 4/prueba.c
+++ TP 2/PARTE 4/prueba.c
@@ -1,44 +1,65 @@
-#include <ctype.h>
 #include <stdio.h>
+/*5. Se ingresan N números enteros, presentar por pantalla un mensaje indicando cuál es el
+promedio de los impares. No considerar el número cero como número impar. Resolver
+empleando funciones:
+○       Controlar que el valor de N con la función solicitarCantidad().
+○       Usar además una función para determinar si un número es impar
+○       Usar una función para calcular el promedio de solicitado*/
 
-int isPosit(int numero);
-void control(int numero);
+int ingresaNumeroPos();
+int esPar(int NUMERO);
+void sumaImpares(int n);                   // cuando pasamos de paso de parmetro por  valor a referencia recordar cambiar los parametros de declaracion y definicion acordemente
+float calcularPromedio(float *, float *);  // cuando cambies la funcion de paso por valor a paso por referencia recordar cambiar el input en la declaracion de la funcion
 
-int main(int argc, char const *argv[]) {
-    int numero;
-    char letra;
-
-    do {
-        printf("Ingrese la cantidad de letras que va a ingresar: ");
-        scanf("%i", &numero);
-    } while (isPosit(numero) == 0);
-    while (getchar() != '\n')
-        ;
-    control(numero);
-
+int main() {
+    int n, suma_impares;
+    float promedio_impares;
+    printf("ingrese la cantidad (positiva) de numeros a verificar");
+    n = ingresaNumeroPos();
+    // suma_impares=sumaImpares(n);elimino esta linea porque al hacer el calcula de suma_impares y de total en una sola funcion(que no puede retornar ambos valores) en vez de pasarselos por valor al main se los paso por referencia a la funcion que calcula el promedio
+    // promedio_impares=calcularPromedio(suma_impares,total); recibo suma_impares y total por referencia con la funcion suma_impares
+    printf("el promedio  de los numeros impares es %f", promedio_impares);  // retornado por valor con la funcion calcularPromedio
     return 0;
 }
-int isPosit(int numero) {
-    int bandera = 0;
-    if (numero > 0) {
-        bandera = 1;
-    }
-    return bandera;
-}
-void control(int numero) {
-    char letra;
-    int contador;
-    for (contador = 0; numero > contador; contador++) {
-        printf("Ingrese una letra: ");
-        scanf("%c", &letra);
-        getchar();
-        if (isalpha(letra)) {
-            if (letra == 'A' || letra == 'E' || letra == 'I' || letra == 'O' || letra == 'U' || letra == 'a' || letra == 'e' || letra == 'i' || letra == 'o' || letra == 'u') {
-                letra = toupper(letra);
-            } else {
-                letra = tolower(letra);
-            }
-            printf("\n> %c\n", letra);
+
+int ingresaNumeroPos() {
+    int NUMERO;
+    do {
+        scanf("%d", &NUMERO);
+        if (NUMERO <= 0) {
+            printf("el numero no es positivo");
         }
     }
+
+    while (NUMERO <= 0);
+    return NUMERO;
+}
+
+int esPar(int NUMERO) {
+    if (NUMERO % 2 == 0)
+        return 1;
+    else {
+        return 0;
+    }
+}
+
+void sumaImpares(int n) {
+    int suma_impares = 0;
+    int total = 0;
+    int NUMERO;
+    for (int i = 1; i <= n; i++) {
+        printf("ingrese el numero numero %d\n", i);
+        scanf("%d", &NUMERO);
+        if (!esPar(NUMERO)) {
+            suma_impares = suma_impares + NUMERO;
+        }
+        total = total + NUMERO;
+    }
+    calcularPromedio(&suma_impares, &total);  // siempre poner ; despues de las llamadas de funciones
+}
+
+float calcularPromedio(float *p_impares, float *p_total) {  // declaro el puntero con el * para que reciba la direccion de memoria suma_impares y total de la funcion
+
+    float promedio_impares = *p_impares / *p_total * 100;
+    return promedio_impares;
 }
